e4245195fcc3b92ff4c2924268f8088ae1781386
VentCat <73477803+dsxksss@users.noreply.github.com>
2022-12-08 16:56:39 +0800

    add 注释

--- index.js
+++ index.js
@@ -7,8 +7,14 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
+// 监听连接触发器
+// 如果被触发则运行以下代码
 io.on("connection", (socket) => {
+  console.log("一个用户进入房间");
+
+  //  当前端触发该触发器时 给前端触发器发送msg
   socket.on("chat message", (msg) => {
+    console.log("触发js msg:" + msg);
     io.emit("chat message", msg);
   });
 });
